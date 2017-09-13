20f5b55a786da12de25cce330b42464b0155a550
AdrieneMM <ad.marianas@hotmail.com>
2017-09-13 12:17:02 -0300

    quase

--- ExercicioListActivity/app/src/main/java/com/example/alunos/exerciciolistactivity/MainActivity.java
+++ ExercicioListActivity/app/src/main/java/com/example/alunos/exerciciolistactivity/MainActivity.java
@@ -4,6 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import java.util.Scanner;
+
+import com.example.alunos.exerciciolistactivity.model.Pessoa;
+
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,4 +23,19 @@ public class MainActivity extends AppCompatActivity {
         Intent it = new Intent(this, mostrarListaDinamica.class);
         startActivity(it);
     }
+    private static Scanner s;
+    public void salvar(View v){
+        ArrayList<Pessoa> lista = new ArrayList<Pessoa>();
+        Pessoa pessoa = new Pessoa();
+
+        s = new Scanner(System.in);
+        EditText nomeBox = (EditText)findViewById(R.id.nomeBox);
+        String nomeB = nomeBox.getText().toString();
+        EditText telefoneBox = (EditText)findViewById(R.id.telefoneBox);
+        String telefoneB = telefoneBox.getText().toString();
+        EditText idadeBox = (EditText)findViewById(R.id.idadeBox);
+        String idadeB = idadeBox.getText().toString();
+
+    }
+
 }
